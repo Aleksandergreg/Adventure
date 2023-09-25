3cb3d0debe9bab8d07e1720c077222fe9159ed52
Aleksandergreg <alek3691@stud.kea.dk>
2023-09-25 18:40:19 +0200

    Made UI class, made switch case for UserInput and added info line

--- src/main/java/Adventure.java
+++ src/main/java/Adventure.java
@@ -1,12 +1,44 @@
 public class Adventure {
-    private Room current;
     private Room room1;
+    private Room room2;
+    private Room room3;
+    private Room room4;
+    private Room room5;
+    private Room room6;
+    private Room room7;
+    private Room room8;
+    private Room room9;
+
+    private Room currentRoom;
+
 
     public Adventure(){
-room1 = new Room("Rum 1", "Stor guldskat");
-current = room1;
+        Room room1 = new Room("Room 1", "The brown bear is lurking");
+        Room room2 = new Room("Room 2", "The grizzly bear is lurking");
+        Room room3 = new Room("Room 3", "The polar bear is lurking");
+        Room room4 = new Room("Room 4", "The polar bear is lurking");
+        Room room5 = new Room("Room 5", "The polar bear is lurking");
+        Room room6 = new Room("Room 6", "The polar bear is lurking");
+        Room room7 = new Room("Room 7", "The polar bear is lurking");
+        Room room8 = new Room("Room 8", "The polar bear is lurking");
+        Room room9 = new Room("Room 9", "The polar bear is lurking");
+
+        currentRoom = room1;
     }
 public Room getCurrent(){
-    return current;
+    return currentRoom;
+}
+public void setForRooms(){
+        //Setters for each room connecting them
+        //Room 1
+room1.setNorthRoom(room2);
+room1.setSouthRoom(room4);
+        //Room 2
+    room2.setEastRoom(room3);
+    room2.setWestRoom(room1);
+    //room 3
+    room3.setWestRoom(room2);
+    room3.setSouthRoom(room6);
+    //room 4
 }
 }
--- src/main/java/UserInterfae.java
+++ src/main/java/UserInterfae.java
@@ -3,21 +3,30 @@ import java.util.Scanner;
 public class UserInterfae {
     private Scanner input = new Scanner(System.in);
     public Adventure adventure = new Adventure();
+
     public void start() {
         System.out.println("Welcome to the adventure game");
-        String UserInput = input.nextLine();
-        switch (UserInput){
+
+
+
+        String userInput = "";
+       while (!userInput.equals("exit")){
+           userInput = input.nextLine().toLowerCase();
+        switch (userInput){
             case "north", "n" -> System.out.println("Going north");
-            case  "south" -> System.out.println("Going south");
-            case "east" ->System.out.println("Going east");
-            case "west" -> System.out.println("Going west");
-            case "look" -> adventure.getCurrent();
-            case "exit" -> System.exit(0);
+            case  "south", "s" -> System.out.println("Going south");
+            case "east", "e" ->System.out.println("Going east");
+            case "west", "w" -> System.out.println("Going west");
+            case "look", "l" ->{ System.out.println(adventure.getCurrent().getDescription());
+                System.out.println("You are inside of "+ adventure.getCurrent().getName());
+            break;}
+            case "exit" -> { System.exit(0);}
             case "info" -> System.out.println("""
                             Write north, south, east or west to move in this dirrection
                             Write look to view your current room 
                             Write exit to exit the game
                             """);
         }
+       }
     }
 }
